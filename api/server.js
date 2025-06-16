3ec562daf43b2fc3dab7228c672d91c89e3f71b8
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-15 20:19:37 -0700

    add 404 message

--- api/server.js
+++ api/server.js
@@ -38,7 +38,7 @@ server.get('/api/pets', async (req, res, next) => {
 
 // [CATCH-ALL]
 server.use('*', (req, res, next) => {
-  next({ status: 404 })
+  next({ status: 404, message: 'Not found' })
 })
 
 // Error handling middleware
